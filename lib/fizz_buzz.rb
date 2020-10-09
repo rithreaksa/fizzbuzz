5c245d83cef9bf6aba3b2c3a9f89593a93248322
Rithreaksa <wonderfulreaksa@gmail.com>
2020-10-09 11:01:54 +0200

    Writing first Rspec test and defining function for fizz_buzz

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,4 @@
+def fizz_buzz(number)
+  number
+end
+
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,2 +1,9 @@
 require './lib/fizz_buzz'
 
+describe "fizz_buzz" do
+  it "returns the given number that is not specified in the objective" do
+    actual = fizz_buzz(1)
+    expected = 1
+    expect(actual). to(eq(expected))
+  end
+end
